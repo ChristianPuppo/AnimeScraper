1c4f3bed7ef0e63321bd47e383dae90738481147
Christian Puppo <christianpuppo@192.168.0.14>
2024-09-26 15:46:37 -0300

    m3u

--- app.py
+++ app.py
@@ -1,51 +1,52 @@
 from flask import Flask, render_template, request, jsonify, Response
-import requests
+import asyncio
 from bs4 import BeautifulSoup
 import re
 from urllib.parse import urljoin
-from Scraper import get_episodes, get_video_urls
+from Scraper import get_episodes, get_video_urls, search_anime
 
 app = Flask(__name__)
 
 BASE_URL = "https://www.animesaturn.mx"
 
-def search_anime(query):
-    search_url = urljoin(BASE_URL, f"/animelist?search={query}")
-    response = requests.get(search_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'html.parser')
-    results = soup.find_all('a', class_='badge-archivio')
-    return [{"title": result.text.strip(), "url": urljoin(BASE_URL, result['href'])} for result in results]
+def run_async(func):
+    def wrapper(*args, **kwargs):
+        return asyncio.run(func(*args, **kwargs))
+    return wrapper
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
-def search():
+@run_async
+async def search():
     query = request.form['query']
-    results = search_anime(query)
+    results = await search_anime(query)
     return jsonify(results)
 
 @app.route('/search_suggestions', methods=['POST'])
-def search_suggestions():
+@run_async
+async def search_suggestions():
     query = request.form['query']
-    results = search_anime(query)[:10]  # Limita a 10 suggerimenti
-    return jsonify(results)
+    results = await search_anime(query)
+    return jsonify(results[:10])  # Limita a 10 suggerimenti
 
 @app.route('/episodes', methods=['POST'])
-def episodes():
+@run_async
+async def episodes():
     anime_url = request.form['anime_url']
     print(f"Richiesta per gli episodi di: {anime_url}")
-    episodes = get_episodes(anime_url)
+    episodes = await get_episodes(anime_url)
     print(f"Episodi trovati: {episodes}")
     return jsonify(episodes)
 
 @app.route('/stream', methods=['POST'])
-def stream():
+@run_async
+async def stream():
     episode_url = request.form['episode_url']
     print(f"Richiesta per lo streaming dell'episodio: {episode_url}")
-    video_urls = get_video_urls([episode_url])
+    video_urls = await get_video_urls([episode_url])
     if video_urls and video_urls[0]:
         print(f"URL video estratto: {video_urls[0]}")
         return jsonify({"video_url": video_urls[0]})
